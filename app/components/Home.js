3e2a4e23b41dcd50cd9731646ca806624f3f84d6
Yasser L'HOCINE <yasser.lhocine@seloger.com>
2017-03-24 16:48:04 +0100

    add svg test

--- app/components/Home.js
+++ app/components/Home.js
@@ -5,6 +5,23 @@ require('../main.css');
 function Home() {
     return (
     <div className="home">
+    <svg width="200" height="200" viewBox="0 0 200 200">
+    <defs>
+        <mask id="mask" x="0" y="0" width="120" height="120">
+        <circle cx="100" cy="50" r="50" fill="#fff"/>
+        <path id="path1" d="m -15 0 a 15.267858,12.428572 0 1 1 30.53572,0 15.267858,12.428572 0 1 1 -30.53572,0 z" />
+        <circle cx="100" cy="50" r="16">
+        <animateMotion 
+            begin="0s" 
+            dur="3s" 
+            repeatCount="indefinite">
+            <mpath xlinkHref="#path1" />
+        </animateMotion>                    
+        </circle>
+        </mask>
+    </defs>
+    <circle cx="100" cy="50" r="30" mask="url(#mask)" fillOpacity="1"/>
+    </svg>
         <SearchContainer />
     </div>);
 };
